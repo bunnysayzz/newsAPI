b61f9bc1c3c746df31b262ed820d947eab7edbc5
bunnysayzz <stfuazzo@gmail.com>
2024-01-20 09:02:38 +0530

    css module added

--- src/components/NewsApp/News.js
+++ src/components/NewsApp/News.js
@@ -1,30 +1,29 @@
-import React, { useEffect, useState } from 'react'
-import Newsitem from './Newsitem'
-import "./News.css"
+import React, { useEffect, useState } from 'react';
+import Newsitem from './Newsitem';
+import styles from './News.module.css';  // Use import with 'styles' alias
+
 export default function News() {
-    const [data,setData] = useState([]);
-       
-    useEffect(()=>{
-        const fetchData = async()=>{
-            const data =await fetch("https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=459cc0da51964312a4de166a80b43f72")
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const data = await fetch("https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=459cc0da51964312a4de166a80b43f72");
             const response = await data.json();
-            setData(response.articles)
-           }
+            setData(response.articles);
+        }
         fetchData();
-    },[])
-  return (
-    <>
-    <div className='main'>
-    
-        <ul className='container'>
-            {data.map((item,index)=>(
-                <Newsitem key={index} title={item.title} description={item.description}
-                img ={item.urlToImage} url = {item.url}/>
-            ))}
-        </ul>
-    </div>
-    
-    </>
-    
-  )
+    }, []);
+
+    return (
+        <>
+            <div className={styles.main}>  {/* Use the 'styles' object to reference the class */}
+                <ul className={styles.container}>  {/* Use the 'styles' object to reference the class */}
+                    {data.map((item, index) => (
+                        <Newsitem key={index} title={item.title} description={item.description}
+                            img={item.urlToImage} url={item.url} />
+                    ))}
+                </ul>
+            </div>
+        </>
+    );
 }
